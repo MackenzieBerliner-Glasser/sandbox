78dd0e819a67a34093d0c359b7fc19035aa40fc2
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-04-01 10:22:49 -0700

    simple tdd example

---
+++ test/double.test.js
@@ -0,0 +1,9 @@
+const double = require('../double');
+
+describe('double', () => {
+  it('returns a num doubled', () => {
+    const expected = 4;
+    const result = double(2);
+    expect(expected).toEqual(result);
+  })
+});
