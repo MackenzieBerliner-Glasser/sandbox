f11f647229c037dea60c0d9efea7c6590577e7ab
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-04-01 11:33:03 -0700

    testing for reduce function

---
+++ add.js
@@ -0,0 +1,5 @@
+const addArr = arr => arr.reduce((a, c) => a + c, 0);
+
+module.exports = {
+  addArr
+};
---
+++ test/add.test.js
@@ -0,0 +1,10 @@
+const { addArr } = require('../add');
+
+describe('add array elements', () => {
+  it('returns a sum of all elements in an array', () => {
+    const arr = [1, 2, 3];
+    const expected = 6;
+    const result = addArr(arr);
+    expect(expected).toEqual(result);
+  });
+});
