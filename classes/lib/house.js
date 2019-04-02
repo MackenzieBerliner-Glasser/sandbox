0465026ba9d204c713fc209c7c195bb6856ce15a
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-04-02 12:24:27 -0700

    added testing coverage

--- classes/lib/house.js
+++ classes/lib/house.js
@@ -1,9 +1,14 @@
 class House {
-  constructor(location, floors, bedrooms, bathrooms) {
+  constructor(location, floors, bedrooms, bathrooms, price) {
     this.location = location;
     this.floors = floors;
     this.bedrooms = bedrooms;
     this.bathrooms = bathrooms;
+    this.price = price;
+  }
+
+  currentPrice() {
+    return `the price is ${this.price}`;
   }
 }
 
--- classes/test/house.test.js
+++ classes/test/house.test.js
@@ -1,8 +1,23 @@
 const { House } = require('../lib/house');
 
 describe('testing for the house class', () => {
+  const house = new House('123 elm st.', 4, 3, 6, '200,000');
   it('tests that house has a location property', () => {
-    const house = new House('123 elm st.', 4, 3, 6);
     expect(house.location).toEqual('123 elm st.');
+  });
+
+  it('tests that house has a new floor property', () => {
+    expect(house.floors).toEqual(4);
+  });
+
+  it('tests that house has a bedroom property', () => {
+    expect(house.bedrooms).toEqual(3);
+  });
+
+  it('tests that house has a new bathroom property', () => {
+    expect(house.bathrooms).toEqual(6);
+  });
+  it('tests that houses price method returns a random price', () => {
+    expect(house.currentPrice()).toEqual('the price is 200,000');
   });
 });
